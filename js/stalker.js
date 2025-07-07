452ae128958473b9f1e3d48ed36fdd35389ac8e3
Yoshida <sunnri257@gmail.com>
2025-07-07 21:53:06 +0900

    課題修正

--- js/stalker.js
+++ js/stalker.js
@@ -2,14 +2,17 @@ $(function () {
   // id[js-stalker]を取得する
   const stalker = $("#js-stalker");
   // 不透明にする
-  $(".stalker").css("opacity","1");
+  $(stalker).css("opacity","1");
   // マウスを移動させたときの処理を指定する
-  $(document).on("mousemove", function (update) {
+  $(document).on("mousemove", function update (e) {
     // マウスの座標を取得
-    const x = update.clientX;
-    const y = update.clientY;
+    const x = e.clientX;
+    const y = e.clientY;
     // ストーカーの位置を更新する
-    $(".stalker").css("transform","translate(" + x + "px, " + y + "px)");
+    $(stalker).css("transform","translate(" + x + "px, " + y + "px)");
+    // 次のフレームの描画を予約
+    requestAnimationFrame(update);
   });
+  // アニメーションの開始
   requestAnimationFrame(update);
 });
